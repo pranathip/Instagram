37a31f982721b4a649355f116ca480007a44790a
Pranathi Peri <pranathiperi@fb.com>
2020-07-06 14:00:36 -0500

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Pranathi Peri on 7/6/20.
+//  Copyright © 2020 Pranathi Peri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instagram
+//
+//  Created by Pranathi Peri on 7/6/20.
+//  Copyright © 2020 Pranathi Peri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
